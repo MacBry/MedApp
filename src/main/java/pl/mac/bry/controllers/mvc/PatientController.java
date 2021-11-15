a51da0b73b077376101e945eeabd5f91cbc02409
MacBry <maciej.bryja@wp.pl>
2021-11-15 14:57:59 +0100

    patients work

--- src/main/java/pl/mac/bry/controllers/mvc/PatientController.java
+++ src/main/java/pl/mac/bry/controllers/mvc/PatientController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import pl.mac.bry.services.PatientService;
 
@@ -21,6 +24,13 @@ public class PatientController {
 	@GetMapping("/show-patients")
 	public String showAllPatients(Model model) {
 		model.addAttribute("patients", patientService.getAllPatients());
+		return "patients";
+	}
+	
+	@PostMapping("/show-patients")
+	public String findPatientByPESEL(Model model, @RequestParam(defaultValue= "") String pesel) {
+		System.out.println("pesel");
+		model.addAttribute("patients", patientService.findPatientByPesel(pesel));
 		return "patients";
 	}
 }
