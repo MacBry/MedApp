1057b51d08d627237c68df081250a37df32bd1fa
Maciej Bry <maciej.bryja@wp.pl>
2022-02-12 17:25:42 +0100

    add service to referral units

---
+++ src/main/java/pl/mac/bry/services/ReferralUnitService.java
@@ -0,0 +1,27 @@
+package pl.mac.bry.services;
+
+import java.util.List;
+
+import com.google.common.base.Function;
+
+import pl.mac.bry.entities.ReferralUnit;
+
+public interface ReferralUnitService {
+	
+	ReferralUnit findReferralUnitById(long id);
+	
+	ReferralUnit findReferralUnitByFullName(String fullName);
+	
+	ReferralUnit finReferralUnitByShortName(String shortName);
+	
+	Iterable<ReferralUnit> getAllReferralUnits();
+	
+	void addReferralUnit (ReferralUnit referralUnit);
+	
+	void updateReferralUnit (ReferralUnit referralUnit);
+	
+	void deleteReferralunit (long id);
+	
+	<T, O> List<T> getValues(Class<T> clazz, List<O> listToExtractFrom, Function<O, T> extractor);
+	
+}
