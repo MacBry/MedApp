3d7aebbb62a8311aac2749011df7f6ffa2b4e3b4
Maciej Bry <maciej.bryja@wp.pl>
2022-02-12 20:10:50 +0100

    servoice

---
+++ src/main/java/pl/mac/bry/services/impl/ReferralunitserviceImpl.java
@@ -0,0 +1,60 @@
+package pl.mac.bry.services.impl;
+
+import java.util.List;
+
+import com.google.common.base.Function;
+
+import pl.mac.bry.entities.ReferralUnit;
+import pl.mac.bry.services.ReferralUnitService;
+
+public class ReferralunitserviceImpl implements ReferralUnitService {
+
+	@Override
+	public ReferralUnit findReferralUnitById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReferralUnit findReferralUnitByFullName(String fullName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReferralUnit finReferralUnitByShortName(String shortName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<ReferralUnit> getAllReferralUnits() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addReferralUnit(ReferralUnit referralUnit) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateReferralUnit(ReferralUnit referralUnit) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteReferralunit(long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T, O> List<T> getValues(Class<T> clazz, List<O> listToExtractFrom, Function<O, T> extractor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
