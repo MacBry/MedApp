46ca8f46c63576b2954c86f62b85541294150153
Maciej Bry <maciej.bryja@wp.pl>
2022-02-14 18:39:09 +0100

    Rest for Referral Unit

--- src/main/java/pl/mac/bry/controllers/rest/ReferralUnitRESTController.java
+++ src/main/java/pl/mac/bry/controllers/rest/ReferralUnitRESTController.java
@@ -3,14 +3,20 @@ package pl.mac.bry.controllers.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.itextpdf.text.pdf.PdfStructTreeController.returnType;
 
+import pl.mac.bry.entities.Address;
 import pl.mac.bry.entities.ReferralUnit;
+import pl.mac.bry.entities.ReferralUnitDetails;
 import pl.mac.bry.services.ReferralUnitService;
 
 @RestController
@@ -56,4 +62,42 @@ public class ReferralUnitRESTController {
 		List<ReferralUnit> allReferralUnitsList = (List<ReferralUnit>)referralUnitService.getAllReferralUnits();
 		return referralUnitService.getValues(String.class, allReferralUnitsList, ReferralUnit::getShortName);
 	}
+	
+	@GetMapping("/id/{id}/address")
+	public Address getReferralUnitAddress(@PathVariable long id) {
+		return referralUnitService.findReferralUnitById(id).getAddress();
+	}
+	
+	@GetMapping("/addresses")
+	public List<Address> getAllReferralUnitsAddresses() {
+		List<ReferralUnit> allReferralUnitsList = (List<ReferralUnit>)referralUnitService.getAllReferralUnits();
+		return referralUnitService.getValues(Address.class, allReferralUnitsList, ReferralUnit::getAddress);
+	}
+	
+	@GetMapping("/id/{id}/detail")
+	public ReferralUnitDetails getReferralUnitDetails(@PathVariable long id) {
+		return referralUnitService.findReferralUnitById(id).getReferralUnitDetails();
+	}
+	
+	@GetMapping("/details")
+	public List<ReferralUnitDetails> getAllReferralUnitsDetails () {
+		List<ReferralUnit> allReferralUnitsList = (List<ReferralUnit>)referralUnitService.getAllReferralUnits();
+		return referralUnitService.getValues(ReferralUnitDetails.class, allReferralUnitsList, ReferralUnit::getReferralUnitDetails);
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void saveReferralUnit(@RequestBody ReferralUnit referralUnit) {
+		referralUnitService.addReferralUnit(referralUnit);
+	}
+	
+	@PutMapping(consumes =  MediaType.APPLICATION_JSON_VALUE)
+	public void updateReferralUnit(@RequestBody ReferralUnit referralUnit) {
+		referralUnitService.updateReferralUnit(referralUnit);
+	}
+	
+	@DeleteMapping("/delete/id/{id}")
+	public void deleteReferralUnit(@PathVariable long id) {
+		referralUnitService.deleteReferralunit(id);
+	}
 }
+
