b3bb5a5032364bb9b28614a410e83f2153d4ee75
Maciej Bry <maciej.bryja@wp.pl>
2022-03-06 09:37:10 +0100

    zmiany

--- src/main/java/pl/mac/bry/MedAppApplication.java
+++ src/main/java/pl/mac/bry/MedAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MedAppApplication {
-// dzis poczułem ze to co tutaj robie jest chuj wart
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MedAppApplication.class, args);
